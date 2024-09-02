52466831a91ee2f62ebe8b61220e0817b3581c60
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-02 23:35:31 +0530

    variables in javascript;

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+/*
+## 01 variables
+
+const - cannoted be changed
+
+let
+
+var - we should not use var because of scope problem
+
+use console.table([]);// To print all vales at once
+*/
+
+var name1 = "Manjunath"
+let age = 26
+const sex = "male"
+var1 = "hii"
+
+console.table([name1,age,sex,var1])
