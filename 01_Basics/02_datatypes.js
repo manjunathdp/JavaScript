71b653d71dc7deaff492dee04571fdb2e72a9a03
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-02 23:45:02 +0530

    data types in javascript;

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,36 @@
+/*
+## 2 dataTypes:
+
+"use strict";// treat all JS code as newer versionprimitive 
+
+datatypes-
+
+number
+
+bigInt
+
+string
+
+boolean
+
+null
+
+undefined
+
+symbol
+
+non-primitive
+
+object
+
+// to know the type of the variable us type of 
+*/ 
+let a = 1;
+let b = "Abc"
+let c = true
+let d = null
+let e;
+
+console.table([a,b,c,d,e])
+console.table([typeof a, typeof b,typeof c,typeof d,typeof e])
+
