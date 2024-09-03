9a7f246b7dc729194cd19665987190518004bf1b
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-03 23:39:36 +0530

    Conversion Operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let a = "417"
+
+console.log(a);
+
+console.log(typeof(a));
+
+console.log(typeof Number(a));
+
+console.log(Number(true));
+
+console.log(Boolean(0));
+
+console.log(Boolean(""));
+
+console.log(null);
+
+
+
+
+
