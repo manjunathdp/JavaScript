671f9514c06c7995879535a684897370d604ead7
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-04 19:10:05 +0530

    Memory management added

---
+++ 01_Basics/05_memoryManagement.js
@@ -0,0 +1,7 @@
+
+let userOne = { email: "user@google.com", upi: "user@upi" };
+let userTwo = userOne; // userTwo points to the same object in the heap
+
+let myYouTubeName = "myChannel"; // Primitive value stored in stack
+let anotherName = myYouTubeName; // Another stack variable with the same value
+
