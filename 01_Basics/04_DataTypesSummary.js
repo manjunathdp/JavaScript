770d69d6c969bc051eec475281af41f0c3a06734
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-09-04 18:44:36 +0530

    Data Types Summary

---
+++ 01_Basics/04_DataTypesSummary.js
@@ -0,0 +1,25 @@
+
+let name = "John Doe";              // String
+let age = 30;                       // Number
+let isAvailable = true;             // Boolean
+let data = null;                    // null
+let something;                      // undefined
+let uniqueID = Symbol("id");        // symbol
+let bigNumber = BigInt(1234567890); // BigInt
+let fruits = ["Apple", "Banana"];   // Array
+let person = { name: "Alice" };     // Object
+function greet() {                  // Function
+  return "Hello!";
+}
+
+console.log(typeof name);        // "string"
+console.log(typeof age);         // "number"
+console.log(typeof isAvailable); // "boolean"
+console.log(typeof data);        // "object"
+console.log(typeof something);   // "undefined"
+console.log(typeof uniqueID);    // "symbol"
+console.log(typeof bigNumber);   // "bigint"
+console.log(typeof fruits);      // "object"
+console.log(typeof person);      // "object"
+console.log(typeof greet);       // "function"
+
