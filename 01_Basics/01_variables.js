5cfcee8767f4379de6d012e9809b3d70d3859def
Manjunath Pujar <101979415+manjunathdp@users.noreply.github.com>
2024-11-12 23:09:27 +0530

    Update 01_variables.js

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,14 +1,4 @@
-/*
-## 01 variables
 
-const - cannoted be changed
-
-let
-
-var - we should not use var because of scope problem
-
-use console.table([]);// To print all vales at once
-*/
 
 var name1 = "Manjunath"
 let age = 26
